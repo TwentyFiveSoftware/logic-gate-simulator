2e2ca650ba3c30ec72ae3ab98bec4f053a111e99
TwentyFive <TwentyFiveSoftware@gmail.com>
2020-10-17 17:47:21 +0200

    Updates

---
+++ precache-manifest.6b9f024f1d481c3ec077be32d4a79cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ee7b91361243ee4356385bc80bf942",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c23a5631e7da2a980d1",
+    "url": "/static/css/main.1f554bc7.chunk.css"
+  },
+  {
+    "revision": "28b828f0e4936265845d",
+    "url": "/static/js/2.cd7747f8.chunk.js"
+  },
+  {
+    "revision": "3a2ea33e8a131b8b33cb161570e8658e",
+    "url": "/static/js/2.cd7747f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c23a5631e7da2a980d1",
+    "url": "/static/js/main.93555cbd.chunk.js"
+  },
+  {
+    "revision": "591232adca2e4aad30de",
+    "url": "/static/js/runtime-main.278d2513.js"
+  }
+]);
